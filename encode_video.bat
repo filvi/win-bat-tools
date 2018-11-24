46d09bd57ace666511ecbb67f34d5c1b16c711fe
Filippo Vicari <fil.vicari@gmail.com>
2018-11-24 12:55:43 +0100

    Fix Spazi nel nome e impostazione preset
    - I file con spazi nel nome vengono processati correttamente 
    - Mostra una anteprima di quanto scelto e da la possibilità all'utente di confermare la selezione
    - Impostazione di preset aggiungendo 1080 | 720 | 480 come terzo parametro per impostare il preset a "HQ 1080p30 Surround" | "HQ 720p30 Surround " | "HQ 480p30 Surround"

--- encode_video.bat
+++ encode_video.bat
@@ -1,5 +1,25 @@
 @echo off
-if not "%~3"=="" handb -i %1 -o %~2.mp4 -e x264 -q 22 -O -X %3 && GOTO EOF
-if not "%~2"=="" handb -i %1 -o %~2.mp4 -e x264 -q 22 -O -X 1920 && GOTO EOF
-if not "%~1"=="" handb -i %1 -o %~n1.mp4 -e x264 -q 22 -O -X 1920 && GOTO EOF
-exit
+
+if not "%~3"=="" set preset="HQ %~3p30 Surround"
+if "%~3"=="" set preset="HQ 1080p30 Surround"
+cls
+:choice
+echo.
+echo.
+Echo  Input:       "%~1"
+if not "%~2"=="" Echo  Output:      "%~n2.mp4"
+if "%~2"=="" Echo  Output:      "%~n1.mp4"
+if not "%~3"=="" Echo  Risoluz:     "%3p30"
+if "%~3"=="" Echo  Risoluz:     "1080p30"
+echo  Profilo:     %preset%
+echo.
+echo.
+set /P c=Le impostazioni sono giuste [S/N]?
+if /I "%c%" EQU "S" goto :encode
+if /I "%c%" EQU "N" exit
+goto :choice
+cls
+:encode
+if not "%~2"=="" handb -i %1 -o %~n2.mp4 --preset %preset% && pause && exit
+if not "%~1"=="" handb -i %1 -o %~n1.mp4 --preset %preset% && pause && exit
+
